8a934672cd77b57f84dc3522f96b85708c225402
Emil Mihalić <emilmihalic3@gmail.com>
2025-08-01 02:25:40 +0200

    fixes

--- Rabbit_The_Hood/Assets/Scripts/Enemy/Enemy.cs
+++ Rabbit_The_Hood/Assets/Scripts/Enemy/Enemy.cs
@@ -10,8 +10,19 @@ public class Enemy : MonoBehaviour
 
     private EnemyMovement enemyMovement;
 
+    private Rigidbody rb;
+    private CapsuleCollider capsuleCollider;
+    [SerializeField] private SphereCollider sphereCollider;
+
+    [SerializeField] private GameObject canvas;
+
+    [SerializeField] private ParticleSystem particleSleep;
+
     private void Awake()
     {
+        rb = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+
         enemyMovement = GetComponent<EnemyMovement>();
         animator = GetComponent<Animator>();
     }
@@ -19,7 +30,6 @@ public class Enemy : MonoBehaviour
     {
         currentHealth = maxHealth;
         UpdateHealthUI();
-
     }
 
     public void Damage(float damage)
@@ -31,6 +41,11 @@ public class Enemy : MonoBehaviour
             animator.SetBool("Dead", true);
             enemyMovement.isDead = true;
 
+            rb.isKinematic = true;
+            capsuleCollider.enabled = false;
+            sphereCollider.enabled = false;
+            canvas.SetActive(false);
+            particleSleep.Play();
         }
     }
 
